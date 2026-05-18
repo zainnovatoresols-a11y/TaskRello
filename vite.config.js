bba283a76448a30c4f7a1bf30c305049f35b1805
Zain Ul Abideen <zainzua12@gmail.com>
2026-05-18 17:44:54 +0500

    Chnages to Vite js added file echo.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js',  'resources/js/echo.js', 'resources/js/board.js', 'resources/js/card.js', 'resources/js/list.js' ],
+            input: ['resources/css/app.css', 'resources/js/app.js',  'resources/js/echo.js',  ],
             refresh: true,
         }),
     ],
